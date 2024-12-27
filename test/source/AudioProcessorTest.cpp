279800c9e34f873270026ab55bd82ca09e3802be
Simon Lind <simonlind90@gmail.com>
2024-12-27 14:38:41 +0100

    Setup CMake project
    Fixing: #9

---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,8 @@
+#include <VocalAnalysis/PluginProcessor.h>
+#include <gtest/gtest.h>
+
+namespace audio_plugin_test {
+    TEST(AudioProcessor, InitialTest) {
+        AudioPluginAudioProcessor processor{};
+    }
+} 
